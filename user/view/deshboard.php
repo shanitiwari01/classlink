43745fe8f248e8bb7d9948025c55e7b10c25f620
kanaujiya-ankit <kanaujiya1999@gmail.com>
2010-03-19 15:55:39 +0530

    set the commen headre and footer for user

--- user/view/deshboard.php
+++ user/view/deshboard.php
@@ -1,5 +1,5 @@
  
-<?php include 'file:///C:/xampp/htdocs/classlink/user/view/layout/header.php'; ?>
+<?php include 'file:///C:/xampp/htdocs/classlink/user/layout/header.php'; ?>
 <!-- Page Loader -->
 <div class="page-loader-wrapper">
     <div class="loader">
@@ -11,7 +11,7 @@
 <!-- Overlay For Sidebars -->
 <div class="overlay"></div>
 
-<?php include 'file:///C:/xampp/htdocs/classlink/user/view/layout/footer.php';  ?>
+<?php include 'file:///C:/xampp/htdocs/classlink/user/layout/footer.php';  ?>
 
 <!-- Right Sidebar -->
 <aside id="rightsidebar" class="right-sidebar">
