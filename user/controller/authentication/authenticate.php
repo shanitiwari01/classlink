4fdf91038122c59f7e31c2b979d092345dd0b748
kanaujiya-ankit <kanaujiya1999@gmail.com>
2019-02-01 23:06:30 +0530

    user login

---
+++ user/controller/authentication/authenticate.php
@@ -0,0 +1,12 @@
+<?php
+include('../../../database/dbcon.php');
+$user = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$qry = "INSERT INTO `m_user`( `user_name`, `user_email`, `user_password`) VALUES ('$user' , '$email' , '$password') ";
+$result=mysqli_query($con,$qry);
+if ($result)
+{
+    header('LOCATION:http://classlink.com/admin/view/dashboard.php');
+}
+?>
---
+++ user/view/auth/sign_up.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html class="no-js " lang="en">
+
+<!-- Mirrored from thememakker.com/templates/aero/html/sign-up.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 28 Jan 2019 16:16:43 GMT -->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+
+<title>:: Aero Bootstrap4 Admin :: Sign Up</title>
+<!-- Favicon-->
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<!-- Custom Css -->
+<link rel="stylesheet" href="../../assets/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="../../assets/css/style.min.css">
+</head>
+
+<body class="theme-blush">
+
+<div class="authentication">    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-sm-12">
+                <form class="card auth_form" action="http://classlink.com/user/controller/authentication/authenticate.php" method="post">
+                    <div class="header">
+                        <img class="logo" src="http://classlink.com/user/assets/images/logo.png" alt="" style="width:219px;" >
+                        <h5>Sign Up</h5>
+                        <span>Register a new membership</span>
+                    </div>
+                    <div class="body">
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Username" name="username">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="zmdi zmdi-account-circle"></i></span>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Enter Email" name="email">
+                            <div class="input-group-append">
+                                <span class="input-group-text"><i class="zmdi zmdi-email"></i></span>
+                            </div>
+                        </div>                        
+                        <div class="input-group mb-3">
+                            <input type="text" class="form-control" placeholder="Password" name="password">
+                            <div class="input-group-append">                                
+                                <span class="input-group-text"><i class="zmdi zmdi-lock"></i></span>
+                            </div>                            
+                        </div>
+                        <div class="checkbox">
+                            <input id="remember_me" type="checkbox">
+                            <label for="remember_me">I read and agree to the <a href="javascript:void(0);">terms of usage</a></label>
+                        </div>
+                        <button type="submit"  class="btn btn-primary btn-block waves-effect waves-light">SIGN UP</button>
+                        <div class="signin_with mt-3">
+                            <a class="link" href="sign-in.html">You already have a membership?</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-8 col-sm-12">
+                <div class="card">
+                    <img src="http://classlink.com/user/assets/images/signup.svg" alt="Sign Up" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Jquery Core Js -->
+<script src="http://classlink.com/user/assets/bundles/libscripts.bundle.js"></script>
+<script src="http://classlink.com/user/assets/bundles/vendorscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js --> 
+</body>
+
+</html>
