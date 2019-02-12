5a6b392403bbbd21ed1519ec3c362c26aea7cfb6
kanaujiya-ankit <kanaujiya1999@gmail.com>
2019-02-13 04:24:14 +0530

    delete opraton  of  course

---
+++ user/controller/course/delete.php
@@ -0,0 +1,13 @@
+<?php
+include 'C:/xampp/htdocs/classlink/database/dbcon.php';
+$courseid= $_REQUEST['course_id'];
+$qry="DELETE FROM `user_course` WHERE `course_id`='$courseid'";
+$run=mysqli_query($con,$qry);
+if ($run) {
+    header('LOCATION:http://classlink.com/user/view/deshboard.php');
+}
+else
+{
+    echo 'error';
+}
+?>
