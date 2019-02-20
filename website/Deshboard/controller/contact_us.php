4044edd0052c8309fc1b6caa4363f5ed083f7e7a
kanaujiya-ankit <kanaujiya1999@gmail.com>
2019-02-20 23:06:27 +0530

    name of folder estartup as Deshboard

---
+++ website/Deshboard/controller/contact_us.php
@@ -0,0 +1,23 @@
+<?php
+include 'C:/xampp/htdocs/classlink/database/dbcon.php';
+if (isset($_POST['send']))
+{
+ 
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $subject=$_POST['subject'];
+    $collage=$_POST['collage'];
+    $message=$_POST['message'];
+    $q="INSERT INTO `contact_us`( `name`, `email`, `subject`, `school_name`, `message`) 
+    VALUES ('$name','$email','$subject','$collage','$message')";
+    $result=mysqli_query($con,$q);
+    if ($result) {
+    header('LOCATION:http://classlink.com/website/eStartup/index.php');
+    }
+
+}
+
+
+
+
+?>
