253e73dc2ba718e1b56e7f97fa61d7ece8c6e746
kanaujiya-ankit <kanaujiya1999@gmail.com>
2019-02-21 12:06:39 +0530

    add Course without ajex is complete

---
+++ user/controller/course/addcourse.php
@@ -0,0 +1,11 @@
+<?php
+include 'C:/xampp/htdocs/classlink/database/dbcon.php';
+//$id=$_POST['id'];
+$cname = $_POST['cname'] ;
+$course_dis = $_POST['cdiscription'] ;
+$qry =" INSERT INTO `user_course`( `course_name`, `course_discription`) VALUES ('$cname' , '$course_dis')";
+$result = mysqli_query($con,$qry);
+if ($result) {
+    header('LOCATION:http://classlink.com/user/view/deshboard.php');
+}
+?>
