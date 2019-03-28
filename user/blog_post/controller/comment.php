154537738c18d79b42a06258341c6f56050a9072
kanaujiya-ankit <kanaujiya1999@gmail.com>
2019-03-28 18:03:49 +0530

    comment redirect nahi horaha h

---
+++ user/blog_post/controller/comment.php
@@ -0,0 +1,23 @@
+<?php
+
+
+include('C:/xampp/htdocs/classlink/database/dbcon.php');
+session_start();
+$user_id= $_SESSION['user']['user_id'];
+$post_id=$_POST['post_id'];
+$comment=$_POST['com'];
+
+
+$qry=" INSERT INTO `post_comment`( `post_id`, `user_id`, `comment`) VALUES 
+('$post_id','$user_id','$comment' ) ";
+$run=mysqli_query($con,$qry);
+
+if ($run) {
+    header('LOCATION:http://classlink.com/user/blog_post/view/post_list_view.php');
+}
+
+
+
+
+
+?>
