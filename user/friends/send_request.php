219662eb6ebdf0e1844676d847457019677e4404
kanaujiya-ankit <kanaujiya1999@gmail.com>
2019-04-07 07:50:04 +0530

    only  break the line

--- user/friends/send_request.php
+++ user/friends/send_request.php
@@ -28,7 +28,10 @@ include $_SERVER['DOCUMENT_ROOT'].'/user/layout/header.php'; ?>
                         <div class="col-lg-4 col-md-12">
                             <div class="card mcard_3">
                                 <div class="body">
-                                    <a href="#"><img style="height: 90px;width: 90px;" src="http://classlink.com/assets/images/user_signup/<?php echo $data['recevier_user_image']; ?>" class="rounded-circle shadow " alt="profile-image"></a>
+                                    <a href="#"><img style="height: 90px;width: 90px;" 
+                                    src="http://classlink.com/assets/images/user_signup/
+                                    <?php echo $data['recevier_user_image']; ?>" 
+                                    class="rounded-circle shadow " alt="profile-image"></a>
                                     <h4 class="m-t-10"><?php echo $data['recevier_user_name']; ?></h4>                            
                                     <div class="row">
                                         
